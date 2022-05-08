95a3f958d0220caf966e553f24b9899cb6bbc589
Bonnie Akhavan <bca44@cornell.edu>
2022-05-08 01:20:52 -0400

    Fixed a2c default to be true blank run

--- wordrl/a2c/sumchars.py
+++ wordrl/a2c/sumchars.py
@@ -16,17 +16,20 @@ each letter with 1 or 0
 "gloVe": The embedding conversion matrix using G representations
 "spacy": Embedding using spaCy
 
-"matrix_width" : the length of the embedding before passing into actor or critic 
+"matrix_width" : the length of the embedding before passing into actor or critic, only used in "original_random" and "glove". 
+26*5 is the size of the "original", and 300 is safe for glove for sure
 
 
-"num_actor_layers", "num_critic_layers": number of linear layers for actor and critic. Final linear layer has no ReLU.
+"num_actor_layers", "num_critic_layers": number of linear layers for actor and critic. Final linear layer has no ReLU. 
+Must be at least 1
 
 "glove_dataset" : dataset used to make glove embeddings
+
 """
 
 
 class SumChars(nn.Module):
-    def __init__(self, obs_size: int, word_list: List[str], n_hidden: int = 1, hidden_size: int = 256, embedding_matrix = "original", matrix_width = 26*5, num_actor_layers = 1, num_critic_layers = 1, glove_dataset = "common_crawl_840", glove_demb = 300):
+    def __init__(self, obs_size: int, word_list: List[str], n_hidden: int = 1, hidden_size: int = 256, embedding_matrix = "original", matrix_width = 26*5, num_actor_layers = 1, num_critic_layers = 1, glove_dataset = "common_crawl_840"):
         """
         Args:
             obs_size: observation/state size of the environment
@@ -55,7 +58,7 @@ class SumChars(nn.Module):
             self.words = torch.Tensor(np.random(embedding_width, len(word_list)))
 
         elif embedding_matrix = "glove":
-            embedding_width = glove_demb
+            embedding_width = matrix_width
             g = GloveEmbedding(glove_dataset, d_emb = glove_demb)
             word_emb_array = np.array([g.emb(w) for w in word_list])
             self.words = torch.Tensor(word_emb_array)
