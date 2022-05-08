ba828d6a5acf6d18a4153431ea4c8afee9f46de9
Kelly Lu <ksl87@cornell.edu>
2022-05-08 01:40:32 -0400

    lalalalala

--- entropy/max_entropy.py
+++ entropy/max_entropy.py
@@ -7,21 +7,23 @@ import gym
 
 from wordrl.envs.wordle_env_v2_visualized import WordleEnv_v2_visualized
 from wordrl.envs.wordle_env_v2_visualized import WORDS
-from wordrl.envs.wordle_env_v2_visualized import ANSWERS
+#from wordrl.envs.wordle_env_v2_visualized import ANSWERS
 from wordrl.filepaths import FILE_PATHS
+
+MATCH_MATRIX = os.path.join(FILE_PATHS["ROOT_PATH"], "data/match_matrix.npy")
 
 
 def correctness(guess, answer):
     colors = [0 for x in range(5)]
-    for i in guess:
+    for i in range(len(guess)):
         if guess[i] == answer[i]:
             colors[i] = 2
         elif guess[i] in answer:
             colors[i] = 1
 
 
-def get_matchings(words, answers):
-    matchings = np.zeroes(len(words), len(answers))
+def generate_matchings(words, answers):
+    matchings = np.zeros((len(words), len(answers)))
     for i, word in enumerate(words):
         for j, answer in enumerate(answers):
             matchings[i][j] = correctness(word, answer)
@@ -29,8 +31,16 @@ def get_matchings(words, answers):
     return matchings
 
 
-def get_distributions(words, matchings):
-    unique, counts = np.unique(matchings, return_counts=True)
+def generate_matchings_matrix():
+    matchings_matrix = generate_matchings(WORDS, WORDS)
+    np.save(MATCH_MATRIX, matchings_matrix)
+    return matchings_matrix
+
+
+def get_distribution(words, matchings):
+    distributions = np.zeros((len(words), 243))
+    for word in words:
+        unique, counts = np.unique(matchings, return_counts=True)
 
     matching_inds = np.argsort(unique)
     distributions = counts[matching_inds[:]]
@@ -38,10 +48,19 @@ def get_distributions(words, matchings):
     return distributions
 
 
-class Maximum_Entropy_Agent():
-    def __init__(self):
-        pass
+def get_matchings(words, answers):
+    matchings = MATCH_MATRIX[][]
 
-    def get_action(self):
-        action = optimal_guess
-        return action
+
+def get_entropies(words, answers):
+    distributions = get_distribution(words, get_matchings(words, answers))
+    return entropy(distributions)
+
+
+def get_best_guess(guess):
+    best_guess =
+
+
+if __name__ == "__main__":
+    first_guess = "salet"
+    generate_matchings_matrix()
